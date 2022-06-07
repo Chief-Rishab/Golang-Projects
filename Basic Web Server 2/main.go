377d5b32d3c199a5901890d116964f7611452a72
Chief-Rishab <f20190124@hyderabad.bits-pilani.ac.in>
2022-06-08 02:50:26 +0530

    Working Checked for HTML pages for the webser

--- Basic Web Server 2/main.go
+++ Basic Web Server 2/main.go
@@ -2,10 +2,43 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 	"log"
+	"net/http"
 )
 
-func main(){
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported", http.StatusNotFound)
+		return
+	}
+	fmt.Fprintf(w, "hello!")
 }
 
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	fmt.Fprintf(w, "POST request successful\n")
+	name := r.FormValue("name")
+	add := r.FormValue("address")
+	fmt.Fprintf(w, "Entered values are Name: %s and Address: %s", name, add)
+}
+
+func main() {
+	fileServer := http.FileServer(http.Dir("./")) //FileServer returns a handler that serves HTTP requests
+	//with the contents of the file system rooted at root.
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Printf("Starting server at Port 2000\n")
+	if err := http.ListenAndServe(":2000", nil); err != nil {
+		log.Fatal(err)
+	}
+}
